4b72db64dceed05b3df6ca62c644ef0138045210
Oceane <sautronoceane98@gmail.com>
2019-11-06 12:10:51 +0100

    upload image arrive a s'affichier

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const Express = require("express");
+const express = require("express");
 const BodyParser = require("body-parser");
 const fileUpload = require('express-fileupload');
+const path = require('path');
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
 
 const CONNECTION_URL = "mongodb+srv://oceane08:password974@cluster0-owldh.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "utilisateurs";
 
-var app = Express();
+var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('views engine','ejs');
@@ -15,6 +16,7 @@ app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
 app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
 
 var database, collection;
 
@@ -27,8 +29,13 @@ const users = require('./routes/users');
 
 app.use('/', users)
 
-app.post('/upload', function(req, res) {
-    console.log(req.files.photo); // the uploaded file object
+
+
+//Action upload du fichier image
+app.post('/uploads', function(req, res) {
+    console.log(req.files.photo); //requette.files.nom du file 
+  
+
   });
   app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
@@ -37,3 +44,4 @@ app.post('/upload', function(req, res) {
     useTempFiles : true,
     tempFileDir : '/tmp/'
 }));
+
